0bdbf207a476e96c65f239ff442abf572b89b733
kwonjaehyun-ssafy10th <kwonjaehyun.ssafy@gmail.com>
2023-08-09 13:37:50 +0900

    SWEA 숫자를 정렬하자 Counting Sort

--- Programmers/src/점_찍기/Solution.java
+++ Programmers/src/점_찍기/Solution.java
@@ -1,5 +1,24 @@
 package 점_찍기;
 
-public class Solution {
+class Solution {
+	public long solution(int k, int d) {
+		long answer = 0;
 
+		for (int i = 0; i * k <= d; i++) {
+			for (int j = 0; j * k <= d; j++) {
+				if (distance(i * k, j * k) <= d) {
+					answer++;
+				} else {
+					break;
+				}
+			}
+		}
+		
+		return answer;
+	}
+	
+	public double distance(int x, int y) {
+		return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+	}
+	
 }
