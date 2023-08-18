f0e8e7246b82a93e6223ff055b5bda21f1183370
kwonjaehyun-ssafy10th <kwonjaehyun.ssafy@gmail.com>
2023-08-18 13:54:05 +0900

    SWEA 1948 날짜 계산기

---
+++ SWEA/src/Practice/날짜_계산기_1948/Solution.java
@@ -0,0 +1,39 @@
+package Practice.날짜_계산기_1948;
+
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int[] days = new int[13];
+		for (int i = 1; i < 12; i++) {
+			switch (i) {
+				case 2:
+					days[i + 1] = 28;
+					break;
+				case 4: case 6: case 9: case 11:
+					days[i + 1] = 30;
+					break;
+				default:
+					days[i + 1] = 31;
+			}
+		}
+		
+		for (int i = 2; i < 13; i++) {
+			days[i] += days[i - 1];
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		int T = sc.nextInt();
+		for (int t = 1; t <= T; t++) {
+			int m1 = sc.nextInt(), d1 = sc.nextInt(), m2 = sc.nextInt(), d2 = sc.nextInt();
+			sb.append("#" + t + " " + (days[m2] + d2 - (days[m1] + d1) + 1) + "\n");
+		}
+		System.out.println(sb);
+		
+		sc.close();
+	}
+
+}
---
+++ SWEA/src/Queue/암호생성기_1225/Solution2.java
@@ -0,0 +1,41 @@
+package Queue.암호생성기_1225;
+
+import java.util.Scanner;
+
+public class Solution2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNextInt()) {
+			int t = sc.nextInt();
+			int len = 8;
+			int[] queue = new int[len];
+			for (int i = 0; i < len; i++) {
+				queue[i] = sc.nextInt();
+			}
+			int decrease = 1;
+			int i = 0;
+			while(true) {
+				if (decrease == 6) decrease = 1;
+				queue[i % len] -= decrease++;
+				if (queue[i % len] <= 0) {
+					queue[i % len] = 0;
+					i++;
+					break;
+				}
+				i++;
+			}
+			StringBuilder sb = new StringBuilder();
+			sb.append("#" + t);
+			for (int j = 0; j < len; j++) {
+				sb.append(" " + queue[(i + j) % len]);
+			}
+			System.out.println(sb);
+			
+		}
+		
+		sc.close();
+	}
+
+}
