3bc3296ad4faa4cdece8a9738ee1fdd6aa0850dd
kwonjaehyun-ssafy10th <139312363+kwonjaehyun-ssafy10th@users.noreply.github.com>
2023-10-04 15:42:09 +0900

    [Gold V] Title: 평범한 배낭, Time: 300 ms, Memory: 57240 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt(), K = sc.nextInt();
+		int[][] item = new int[N + 1][2];
+		for (int n = 1; n <= N; n++) {
+			for (int i = 0; i < 2; i++) {
+				item[n][i] = sc.nextInt();
+			}
+		}
+		int[][] dp = new int[N + 1][K + 1];
+		for (int n = 1; n <= N; n++) {
+			int W = item[n][0], V = item[n][1];
+			for (int k = 1; k <= K; k++) {
+				dp[n][k] = dp[n - 1][k];
+				if (W <= k) dp[n][k] = Math.max(dp[n - 1][k], dp[n - 1][k - W] + V);
+			}
+		}
+		System.out.println(dp[N][K]);
+		
+		sc.close();
+	}
+	
+}
