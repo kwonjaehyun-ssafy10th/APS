c5981cb25db3d1a461f6382494d6a3acf9a2e0d9
kwonjaehyun-ssafy10th <139312363+kwonjaehyun-ssafy10th@users.noreply.github.com>
2023-10-20 15:15:05 +0900

    [Bronze V] Title: A+B - 3, Time: 140 ms, Memory: 13080 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		for (int t = 0; t < T; t++) {
+			System.out.println(sc.nextInt() + sc.nextInt());
+		}
+		
+		sc.close();
+	}
+
+}
