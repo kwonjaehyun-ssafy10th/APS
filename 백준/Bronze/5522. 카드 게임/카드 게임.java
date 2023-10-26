a0202a81152220fdf67bf00410991023fedfdfb0
kwonjaehyun-ssafy10th <139312363+kwonjaehyun-ssafy10th@users.noreply.github.com>
2023-10-26 23:26:07 +0900

    [Bronze V] Title: 카드 게임, Time: 120 ms, Memory: 12832 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		long ans = 0;
+		for (int i = 0; i < 5; i++) {
+			ans += sc.nextLong();
+		}
+		System.out.println(ans);
+		
+		sc.close();
+	}
+
+}
