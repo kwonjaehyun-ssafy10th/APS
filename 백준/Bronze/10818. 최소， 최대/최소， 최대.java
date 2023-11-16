6f2a64333c12db21611214384e115044eddb70a8
kwonjaehyun-ssafy10th <139312363+kwonjaehyun-ssafy10th@users.noreply.github.com>
2023-11-16 22:47:05 +0900

    [Bronze III] Title: 최소, 최대, Time: 464 ms, Memory: 117576 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,22 +1,31 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
 		
-		int N = sc.nextInt();
+		st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
 		int max = Integer.MIN_VALUE;
 		int min = Integer.MAX_VALUE;
-		
+		st = new StringTokenizer(br.readLine());
 		for (int n = 0; n < N; n++) {
-			int m = sc.nextInt();
+			int m = Integer.parseInt(st.nextToken());
 			if (m < min) min = m;
 			if (max < m) max = m;
 		}
-		System.out.println(min + " " + max);
-		
-		sc.close();
+		bw.write(min + " " + max);
+		bw.flush();
+		br.close();
+		bw.close();
 	}
-
 }
