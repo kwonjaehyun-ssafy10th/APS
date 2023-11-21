624485848fcc85c9b86214720098edc305a9e504
kwonjaehyun-ssafy10th <139312363+kwonjaehyun-ssafy10th@users.noreply.github.com>
2023-11-21 23:41:41 +0900

    [Bronze V] Title: 새싹, Time: 76 ms, Memory: 11248 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,14 @@
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(str);
+	}
+	
+	static String str = "         ,r'\"7\n"
+			+ "r`-_   ,'  ,/\n"
+			+ " \\. \". L_r'\n"
+			+ "   `~\\/\n"
+			+ "      |\n"
+			+ "      |";
+
+}
