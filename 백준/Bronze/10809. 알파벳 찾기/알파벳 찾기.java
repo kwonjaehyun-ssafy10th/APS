be21bb2429b090f6a0f9e9a338c2f2113c2f7619
kwonjaehyun-ssafy10th <139312363+kwonjaehyun-ssafy10th@users.noreply.github.com>
2023-12-13 22:12:58 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 104 ms, Memory: 12788 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.next();
+		for (int i = 97; i < 123; i++) {
+			System.out.print(str.indexOf((char) i) + " ");
+		}
+		
+		sc.close();
+	}
+
+}
