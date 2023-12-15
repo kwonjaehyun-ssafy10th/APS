26ede83c720ffd5d650bf9c7d14c3db91329e96c
kwonjaehyun-ssafy10th <139312363+kwonjaehyun-ssafy10th@users.noreply.github.com>
2023-12-15 23:53:37 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 68 ms, Memory: 11188 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,13 @@
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(str);
+	}
+	
+	static String str = "       _.-;;-._\n"
+			+ "'-..-'|   ||   |\n"
+			+ "'-..-'|_.-;;-._|\n"
+			+ "'-..-'|   ||   |\n"
+			+ "'-..-'|_.-''-._|";
+
+}
