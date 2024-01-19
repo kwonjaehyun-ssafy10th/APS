97d3a97d3c1a5b782432551143b6fd3c66a3ec4e
kwonjaehyun-ssafy10th <139312363+kwonjaehyun-ssafy10th@users.noreply.github.com>
2024-01-19 14:00:44 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2080 ms, Memory: 298212 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[10000];
+		for (int n = 0; n < N; n++) {
+			int num = Integer.parseInt(br.readLine());
+			arr[--num]++;
+		}
+		for (int i = 0; i < 10000; i++) {
+			for (int j = 0; j < arr[i]; j++) {
+				bw.write(String.valueOf(i + 1));
+				bw.newLine();
+			}
+		}
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+
+}
