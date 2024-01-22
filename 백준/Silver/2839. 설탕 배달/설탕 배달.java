0c3a4290e4ccd3709ff1b0940439f621c6d4fc15
kwonjaehyun-ssafy10th <139312363+kwonjaehyun-ssafy10th@users.noreply.github.com>
2024-01-22 11:19:51 +0900

    [Silver IV] Title: 설탕 배달, Time: 108 ms, Memory: 12888 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -8,20 +9,17 @@ public class Main {
 		int ans = 0;
 		int N = sc.nextInt();
 		int[] dp = new int[N + 1];
+		Arrays.fill(dp, N);
 		if (3 <= N) dp[3] = 1;
 		if (5 <= N) dp[5] = 1;
 		for (int n = 6; n < N + 1; n++) {
-			if (dp[n - 3] == 0 && dp[n - 5] == 0) {
+			if (dp[n - 3] == N && dp[n - 5] == N) {
 				continue;
-			} else if (dp[n - 3] == 0) {
-				dp[n] = dp[n - 5] + 1;
-			} else if (dp[n - 5] == 0) {
-				dp[n] = dp[n - 3] + 1;
 			} else {
 				dp[n] = Math.min(dp[n - 5], dp[n - 3]) + 1;
 			}
 		}
-		if (dp[N] == 0) ans = -1;
+		if (dp[N] == N) ans = -1;
 		else ans = dp[N];
 		System.out.println(ans);
 		
