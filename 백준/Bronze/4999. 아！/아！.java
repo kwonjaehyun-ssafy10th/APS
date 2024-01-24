a207eeca23c1a21a7bed2fdae68032d98f90f00e
kwonjaehyun-ssafy10th <139312363+kwonjaehyun-ssafy10th@users.noreply.github.com>
2024-01-24 22:56:04 +0900

    [Bronze V] Title: 아!, Time: 112 ms, Memory: 12800 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String a = sc.next();
+		String b = sc.next();
+		if (a.length() < b.length()) {
+			System.out.println("no");
+		} else {
+			System.out.println("go");
+		}
+		
+		sc.close();
+	}
+
+}
