e0d32721d147684faec750e231fd70856788425a
kwonjaehyun-ssafy10th <139312363+kwonjaehyun-ssafy10th@users.noreply.github.com>
2024-01-25 17:32:01 +0900

    [Bronze V] Title: 웰컴, Time: 72 ms, Memory: 11196 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,11 @@
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(a);
+	}
+	
+	static String a = ".  .   .\n"
+			+ "|  | _ | _. _ ._ _  _\n"
+			+ "|/\\|(/.|(_.(_)[ | )(/.";
+
+}
