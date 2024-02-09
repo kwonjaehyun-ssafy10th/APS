735f2c4fc58e298a32fff7e5f398b7daf017e124
kwonjaehyun-ssafy10th <139312363+kwonjaehyun-ssafy10th@users.noreply.github.com>
2024-02-10 08:34:01 +0900

    [Bronze V] Title: 콜센터, Time: 72 ms, Memory: 11188 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,18 @@
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(a);
+	}
+	
+	static String a = "     /~\\\n"
+			+ "    ( oo|\n"
+			+ "    _\\=/_\n"
+			+ "   /  _  \\\n"
+			+ "  //|/.\\|\\\\\n"
+			+ " ||  \\ /  ||\n"
+			+ "============\n"
+			+ "|          |\n"
+			+ "|          |\n"
+			+ "|          |";
+
+}
