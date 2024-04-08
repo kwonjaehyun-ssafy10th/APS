e686d7e93dc4253450e418c4da7d6821721e511f
kwonjaehyun-ssafy10th <139312363+kwonjaehyun-ssafy10th@users.noreply.github.com>
2024-04-08 23:37:15 +0900

    [Silver V] Title: 돌 게임, Time: 112 ms, Memory: 12852 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.java
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -6,16 +6,8 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		
 		int N = sc.nextInt();
-		boolean[] dp = new boolean[Math.max(4, N + 1)];
-		dp[1] = true;
-		dp[3] = true;
-		for (int n = 4; n < N + 1; n++) {
-			if (dp[n - 4]) {
-				dp[n] = true;
-			}
-		}
 		String ans = "CY";
-		if (dp[N]) ans = "SK";
+		if (N % 4 == 1 || N % 4 == 3) ans = "SK";
 		System.out.println(ans);
 		
 		sc.close();
