3d4f255b4f4bb9545bb5dc47db925de3889b5703
kwonjaehyun-ssafy10th <139312363+kwonjaehyun-ssafy10th@users.noreply.github.com>
2024-05-09 16:52:08 +0900

    [Bronze III] Title: N M 찍기, Time: 468 ms, Memory: 129324 KB -BaekjoonHub

---
+++ 백준/Bronze/18883. N M 찍기/N M 찍기.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		
+		int N = sc.nextInt(), M = sc.nextInt();
+		for (int n = 0; n < N; n++) {
+			sb.append(n * M + 1 + "");
+			for (int m = 2; m <= M; m++) {
+				sb.append(" " + (n * M + m));
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
+		
+		sc.close();
+	}
+
+}
