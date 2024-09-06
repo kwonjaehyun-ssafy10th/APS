b1d542a08f9c08d551e653ddf679142269d5a778
kwonjaehyun-ssafy10th <139312363+kwonjaehyun-ssafy10th@users.noreply.github.com>
2024-09-06 19:28:56 +0900

    [Silver V] Title: 문서 검색, Time: 96 ms, Memory: 13280 KB -BaekjoonHub

---
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String doc = sc.nextLine();
+		String word = sc.nextLine();
+		int docLength = doc.length();
+		int wordLength = word.length();
+		int ans = 0;
+		int i = 0;
+		while (i <= docLength - wordLength) {
+			int idx = doc.indexOf(word, i);
+			if (idx == -1) break;
+			i = idx + wordLength;
+			ans++;
+		}
+		System.out.println(ans);
+		
+		
+		sc.close();
+	}
+
+}
