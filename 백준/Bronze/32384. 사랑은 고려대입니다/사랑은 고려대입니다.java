aac73e61c33a020dc27d9e9d5e26b33d6f1def75
kwonjaehyun-ssafy10th <139312363+kwonjaehyun-ssafy10th@users.noreply.github.com>
2024-10-06 19:32:57 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 88 ms, Memory: 12836 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		for (int n = 0; n < N; n++) {
+			System.out.print("LoveisKoreaUniversity ");
+		}
+		
+		sc.close();
+	}
+
+}
