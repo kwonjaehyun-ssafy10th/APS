c22b12c71140ad1fbb6900b8e794f6618722a48e
kwonjaehyun-ssafy10th <139312363+kwonjaehyun-ssafy10th@users.noreply.github.com>
2024-11-18 15:14:26 +0900

    [Bronze II] Title: Sascha, Time: 200 ms, Memory: 18924 KB -BaekjoonHub

---
+++ 백준/Bronze/2391. Sascha/Sascha.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = toInt(br.readLine());
+		for (int i = 0; i < n; i++) {
+			String word = br.readLine();
+			int l = word.length();
+			int m = l + 1;
+			String ans = "";
+			int w = toInt(br.readLine());
+			for (int j = 0; j < w; j++) {
+				String dic = br.readLine();
+				int cnt = 0;
+				for (int k = 0; k < l; k++) {
+					if (word.charAt(k) != dic.charAt(k)) cnt++;
+				}
+				if (cnt < m) {
+					m = cnt;
+					ans = dic;
+				}
+			}
+			System.out.println(ans);
+		}
+		
+		
+		
+		br.close();
+	}
+	
+	static int toInt(String string) {
+		return Integer.parseInt(string);
+	}
+
+}
