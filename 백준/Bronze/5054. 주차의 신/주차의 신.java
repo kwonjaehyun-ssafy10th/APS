0d5c11556caa091793055bff79ef30669b5d9f0d
kwonjaehyun-ssafy10th <139312363+kwonjaehyun-ssafy10th@users.noreply.github.com>
2024-12-18 10:23:06 +0900

    [Bronze II] Title: 주차의 신, Time: 124 ms, Memory: 14380 KB -BaekjoonHub

---
+++ 백준/Bronze/5054. 주차의 신/주차의 신.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		for (int i = 0; i < t; i++) {
+			int n = sc.nextInt();
+			int[] arr = new int[n];
+			for (int j = 0; j < n; j++) {
+				arr[j] = sc.nextInt();
+			}
+			Arrays.sort(arr);
+			int m = 2 * (arr[n - 1] - arr[0]);
+			System.out.println(m);
+		}
+		
+		sc.close();
+	}
+
+}
